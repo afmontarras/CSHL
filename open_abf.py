b62f301a4e04d91747ce24cba39a72899539a0ec
afmontarras <afmontarras@gmail.com>
2024-06-11 17:04:08 -0700

    last update

--- open_abf.py
+++ open_abf.py
@@ -2,10 +2,26 @@ import numpy as np
 import pyabf
 import matplotlib.pyplot as plt
 
+def get_protocol(files):
+    protocols = []
+    for f in files:
+        rec = pyabf.ABF(f)
+        protocols.append(rec.protocol)
+    return protocols
+
+def files_from_protocol(prt_name,files):
+    protocols = get_protocol(files)
+    prt_file = []
+    for i,prt in enumerate(protocols):
+        if (prt_name in prt):
+            prt_file.append(files[i])
+    return prt_file
+
 def plot_swps(file,legend=False):
     rec = pyabf.ABF(file)
-    rec.pr
+    prt = rec.protocol
     fig,ax = plt.subplots(2,sharex=True)
+    ax[0].set_title(prt)
     for swpNB in rec.sweepList:
         rec.setSweep(swpNB)
         ax[0].plot(rec.sweepX,rec.sweepY)
@@ -14,6 +30,11 @@ def plot_swps(file,legend=False):
     ax[1].set_xlabel(rec.sweepLabelX)
     ax[0].set_ylabel(rec.sweepLabelY)
     ax[1].set_ylabel(rec.sweepLabelC)
+    ax[0].spines['bottom'].set_visible(False)
+    # ax[0].set_xticks([])
+    for i in range(2):
+        ax[i].spines['right'].set_visible(False)
+        ax[i].spines['top'].set_visible(False)
     if legend:
         ax[1].legend(loc="right")
     fig.tight_layout()
